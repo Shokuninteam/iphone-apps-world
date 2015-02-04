61b42d4081f44e32767bdc76d611f42da7c0df02
Frank Bassard <f.bassard@gmail.com>
2015-02-04 13:01:16 +1300

    Structural angular

---
+++ js/directives.js
@@ -0,0 +1,27 @@
+iphoneApp
+
+.directive('myNavbar', function() {
+  return {
+    templateUrl: 'views/directives/mynavbar.html'
+  };
+})
+
+.directive('myHeader', function() {
+  return {
+    templateUrl: 'views/directives/myheader.html'
+  };
+})
+
+.directive('topTen', function() {
+  return {
+    templateUrl: 'views/directives/topten.html'
+  };
+})
+
+.directive('categories', function() {
+  return {
+    templateUrl: 'views/directives/categories.html'
+  };
+})
+
+;
