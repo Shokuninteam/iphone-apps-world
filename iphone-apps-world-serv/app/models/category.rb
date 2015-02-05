ac958b298d54aa99fc1c8e538c9c8d385fbac066
LittleCatBear <j.huguet9@gmail.com>
2015-02-05 22:48:16 +1300

    model changes

--- iphone-apps-world-serv/app/models/category.rb
+++ iphone-apps-world-serv/app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ActiveRecord::Base
+	has_many :apps
+	validates :name, presence: true
+
 end
--- iphone-apps-world-serv/app/models/top10.rb
+++ iphone-apps-world-serv/app/models/top10.rb
@@ -1,3 +1,4 @@
 class Top10 < ActiveRecord::Base
   belongs_to :idApp
+  has_many :apps
 end
