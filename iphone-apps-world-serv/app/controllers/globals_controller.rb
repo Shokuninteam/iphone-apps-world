8a34789935bac39690edf4b094f69644cb4eeb54
LittleCatBear <j.huguet9@gmail.com>
2015-02-06 16:35:30 +1300

    global request

--- iphone-apps-world-serv/app/controllers/globals_controller.rb
+++ iphone-apps-world-serv/app/controllers/globals_controller.rb
@@ -1,11 +1,15 @@
 class GlobalsController < ApplicationController
 
 	def index
-
-		@top10 = App.joins(:top10s).order("rank")
-		#@top10 = Top10.where(app_id: App.all)
-    	
-		render json: @top10
+		@top10 = App.joins(:top10s).where(released: true).order("rank")
+    	@articles = App.where(released: true).paginate(page: params[:page]).order("updated_at")
+    	@categories = Category.all
+		render :json => {
+			:top10 => @top10,
+  			:articles => @articles,
+  			:categories => @categories,
+			 }
+		
 	end
 
 end
